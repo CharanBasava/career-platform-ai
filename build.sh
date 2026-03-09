449d9426935e6174580a5019df74739bbcf7d23d
Charan-Basava <basavacharan27@gmail.com>
2026-03-09 21:33:30 +0530

    fix render deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Install wkhtmltopdf required for PDF generation
-apt-get update
-apt-get install -y wkhtmltopdf
-
-# Download spaCy English model for resume parsing
 python -m spacy download en_core_web_sm
